344d891fad9f43aa505730c5355cf234b54bb572
Krixs228 <98397765+Krixs228@users.noreply.github.com>
2023-01-16 18:04:27 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-let cells = document.querySelectoreAll('#filed td');
+let cells = document.querySelectorAll('#field td');
 console.log(cells);
 
 let i = 0;
 function start(cells) {
     for (let cell of cells) {
-        cell.addEventListener('click', function () {
+        cell.addEventListener('click', function step() {
             if (i % 2 == 0) {
-                this.textContent = 'x';
+                this.textContent = 'X';
             } else {
                 this.textContent = '0';
             }
             i++;
+            this.removeEventListener('click', step);
+            if (isWinner(cells) == true) {
+                console.log(`Победитель ${this.textContent}`);
+            };
         })
     }
-
-
 }
 
 start(cells);
+
+function isWinner(cells) {
+    let combs = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (let comb of combs) {
+        if (cells[comb[0]].textContent == cells[comb[1]].textContent && cells[comb[1]].textContent == cells[comb[2]].textContent && cells[comb[0]].textContent != '') {
+            return true;
+        }
+    }
+}
